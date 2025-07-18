64d7ced8fa8e977de0704ab1ce0c4c17143af996
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-07-17 19:03:12 -0500

    feat: :sparkles: `repositories`

---
+++ src/repositories/CamperRepository.php
@@ -0,0 +1,7 @@
+<?php
+interface CamperRepository {
+    public function findById(int $id): ?object;
+    public function getAll(): array;
+    public function create(array $data): ?object;
+    public function update(): object;
+}
